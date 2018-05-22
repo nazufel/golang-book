7538e58cb0a4b52db855d2e9bdd0e6d54aeced7b
Ryan Ross <ryanalexander.ross@gmail.com>
2018-05-22 08:19:41 -0400

    go the variadic function.

--- chapter7/variadic.go
+++ chapter7/variadic.go
@@ -5,24 +5,27 @@ import (
 	"sort"
 )
 
-func findLarge(args ...int) int {
-	x := []int{
-		48, 96, 86, 68,
-		57, 82, 63, 70,
-		37, 34, 83, 27,
-		19, 97, 9, 17,
-	}
+func findLarge(nums ...int) int {
+	// sort the list
+	sort.Ints(nums)
 
-	// I know I cheated, but that's what the standard library is for.
-	sort.Ints(x)
-	highest := x[int(len(3))]
+	// assign var highest int, the last value in the sorted array.
+	// [len(nums)-1] subtracts one because lists start at 0
+	highest := nums[len(nums)-1]
 
+	// print the hightest number in the list
+	fmt.Println(highest)
+
+	// go-plus wants me to return at the end. not sure why, but it compiles.
 	return highest
 }
 
 func main() {
-	fmt.Println(findLarge(48, 96, 86, 68,
+	// define list of numbers
+	nums := []int{48, 96, 86, 68,
 		57, 82, 63, 70,
 		37, 34, 83, 27,
-		19, 97, 9, 17))
+		19, 97, 9, 17}
+	// call the findLarge func and feed it an endless list of inputs
+	findLarge(nums...)
 }
