f11b944824048268cfbb6986eb34501cd4d2e8fa
Ryan Ross <ryanalexander.ross@gmail.com>
2018-05-24 10:40:23 -0400

    working structs, interfaces, and methods for the last question in chapter 9.

---
+++ chapter9/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//Circle struct
+type Circle struct {
+	ra float64 // radius of a circle
+}
+
+//Rectangle struct
+type Rectangle struct {
+	h, w float64 // height and width of a rectangle
+}
+
+// Area interface
+type Area interface {
+	findCirArea() float64
+	findRecArea() float64
+}
+
+// Parameter /Circumferance interace
+type Parameter interface {
+	findCirCirc() float64
+	findRecPar() float64
+}
+
+// print the area of a circle
+func findCirArea(c Circle) {
+	fmt.Println(math.Pi * (c.ra * c.ra))
+}
+
+// print the area of a rectangle
+func findRecArea(r Rectangle) {
+	fmt.Println(r.h * r.w)
+}
+
+//calculate the Parameter/Circumferance
+func findCirCirc(c Circle) {
+	fmt.Println((math.Pi * 2) * c.ra)
+}
+
+func findRecPar(r Rectangle) {
+	fmt.Println((r.h * 2) + (r.w * 2))
+}
+
+func main() {
+	c1 := Circle{
+		2.1,
+	}
+
+	p1 := Rectangle{
+		2,
+		2,
+	}
+
+	// I don't like that the print statements are in the methods, will fix later.
+	findCirCirc(c1)
+	findRecPar(p1)
+	findCirArea(c1)
+	findRecArea(p1)
+}
